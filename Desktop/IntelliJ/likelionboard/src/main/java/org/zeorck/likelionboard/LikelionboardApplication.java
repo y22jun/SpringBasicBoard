1114f5d424755ebf4f466cf3deceab5e75b79272
y22jun <tlsdpwns0612@naver.com>
2025-03-21 16:23:59 +0900

    Setting: build.gradle & application.properties 설정

---
+++ Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/LikelionboardApplication.java
@@ -0,0 +1,13 @@
+package org.zeorck.likelionboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionboardApplication.class, args);
+	}
+
+}
---
+++ Desktop/IntelliJ/likelionboard/src/test/java/org/zeorck/likelionboard/LikelionboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zeorck.likelionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
