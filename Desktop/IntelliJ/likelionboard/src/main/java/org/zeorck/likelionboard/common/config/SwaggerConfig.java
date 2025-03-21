b20af6d18c23d7fda0a80571fc6c4a8bd3e76e3e
y22jun <tlsdpwns0612@naver.com>
2025-03-21 17:04:28 +0900

    Feat: Swagger api 구현

---
+++ Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/common/config/SwaggerConfig.java
@@ -0,0 +1,84 @@
+package org.zeorck.likelionboard.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+import static org.springframework.security.config.Elements.JWT;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+    private final Environment environment;
+
+    private static final Map<String, String> PROFILE_SERVER_URL_MAP = Map.of(
+            "local", "http://localhost:8080"
+    );
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement())
+                .servers(initializeServers())
+                .components(components());
+    }
+
+    private SecurityRequirement securityRequirement() {
+        return new SecurityRequirement().addList(JWT);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("BasicBoard API")
+                .description(getDescription());
+    }
+
+    private List<Server> initializeServers() {
+        return PROFILE_SERVER_URL_MAP.entrySet().stream()
+                .filter(entry -> environment.matchesProfiles(entry.getKey()))
+                .map(entry -> openApiServer(entry.getValue(), "BasicBoard API " + entry.getKey().toUpperCase()))
+                .collect(Collectors.toList());
+    }
+
+    private Server openApiServer(String url, String description) {
+        return new Server().url(url).description(description);
+    }
+
+    private Components components() {
+        return new Components().addSecuritySchemes(JWT, securityScheme());
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .name(JWT)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat(JWT);
+    }
+
+    private String getDescription() {
+        return format("""
+				BasicBoard API 입니다.\n\n
+				""",
+                getLoginUrlByProfile("local"), getLoginUrlByProfile("local")
+        );
+    }
+
+    private String getLoginUrlByProfile(String profile) {
+        return PROFILE_SERVER_URL_MAP.get(profile) + "/login";
+    }
+}
