d3039a8321568222362708b1054e660597254f3a
y22jun <tlsdpwns0612@naver.com>
2025-03-21 17:17:04 +0900

    Feat: Member Entity 구현

---
+++ Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/domain/member/domain/Member.java
@@ -0,0 +1,39 @@
+package org.zeorck.likelionboard.domain.member.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.zeorck.likelionboard.common.domain.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Builder
+    public Member(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+}
