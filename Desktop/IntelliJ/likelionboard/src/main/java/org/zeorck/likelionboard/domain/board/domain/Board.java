900671c0c30287d0cfbd63d528767e6f02bdfd05
y22jun <tlsdpwns0612@naver.com>
2025-03-22 21:08:40 +0900

    Feat: Board Entity 구현

---
+++ Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/domain/board/domain/Board.java
@@ -0,0 +1,47 @@
+package org.zeorck.likelionboard.domain.board.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.zeorck.likelionboard.common.domain.BaseTimeEntity;
+import org.zeorck.likelionboard.domain.member.domain.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Integer views;
+
+    @Builder
+    public Board(String title, String content, Integer views) {
+        this.title = title;
+        this.content = content;
+        this.views = views;
+    }
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+}
