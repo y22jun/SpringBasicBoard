903c8b8f8319f130aeebaffc50036997374408f2
y22jun <tlsdpwns0612@naver.com>
2025-03-24 14:00:30 +0900

    Refacor: CommentController Tag 적용

--- Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/domain/comment/presentation/CommentController.java
+++ Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/domain/comment/presentation/CommentController.java
@@ -1,5 +1,8 @@
 package org.zeorck.likelionboard.domain.comment.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +18,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
+@Tag(name = "댓글", description = "댓글 관리 API")
 public class CommentController {
 
     private final CommentService commentService;
 
+    @Operation(summary = "댓글 저장", description = "특정 게시글 안에 댓글을 저장합니다.")
+    @ApiResponse(responseCode = "201")
     @PostMapping("/{boardId}")
     public ResponseEntity<?> save(
             @MemberId Long memberId,
@@ -28,6 +34,8 @@ public class CommentController {
         return new ResponseEntity<>(commentSaveResponse, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "댓글 수정", description = "특정 게시글 안에 있는 댓글을 수정합니다.")
+    @ApiResponse(responseCode = "200")
     @PutMapping("/{commentId}")
     public ResponseEntity<?> update(
             @MemberId Long memberId,
@@ -37,19 +45,23 @@ public class CommentController {
         return new ResponseEntity<>(commentUpdateResponse, HttpStatus.OK);
     }
 
+    @Operation(summary = "댓글 삭제", description = "특정 게시글 안에 있는 댓글을 삭제합니다.")
+    @ApiResponse(responseCode = "204")
     @DeleteMapping("/{commentId}")
     public ResponseEntity<?> delete(
             @MemberId Long memberId,
             @PathVariable("commentId") Long commentId) {
         commentService.delete(memberId, commentId);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "댓글 조회", description = "특정 게시글 안에 있는 댓글을 모두 조회합니다.")
+    @ApiResponse(responseCode = "200")
     @GetMapping("/{boardId}")
     public ResponseEntity<List<CommentInfoResponse>> getCommentsByBoardId(
             @PathVariable("boardId") Long boardId) {
         List<CommentInfoResponse> comments = commentService.getCommentsByBoardId(boardId);
-        return ResponseEntity.ok(comments);
+        return new ResponseEntity<>(comments, HttpStatus.OK);
     }
 
 }
