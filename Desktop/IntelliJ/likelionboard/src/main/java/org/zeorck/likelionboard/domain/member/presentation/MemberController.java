f0db3ee7938ea4c2c5c1955665cc6ec4ef29c2ff
y22jun <tlsdpwns0612@naver.com>
2025-03-24 13:43:46 +0900

    Refactor: MemberController Tag 적용

--- Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/domain/member/presentation/MemberController.java
+++ Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/domain/member/presentation/MemberController.java
@@ -1,5 +1,8 @@
 package org.zeorck.likelionboard.domain.member.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +15,26 @@ import org.zeorck.likelionboard.domain.member.presentation.response.MemberSaveRe
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
+@Tag(name = "멤버", description = "멤버 관리 API")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/save")
+    @Operation(summary = "회원가입", description = "회원가입을 합니다.")
+    @ApiResponse(responseCode = "201")
+    @PostMapping
     public ResponseEntity<?> save(@RequestBody MemberSaveResponse memberSaveResponse) {
         memberService.save(memberSaveResponse);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(memberSaveResponse, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{memberId}")
-    public ResponseEntity<?> update(@MemberId Long memberId, @RequestBody MemberNicknameUpdateResponse memberNicknameUpdateResponse) {
+    @Operation(summary = "멤버 닉네임 수정", description = "멤버 닉네임을 수정합니다.")
+    @ApiResponse(responseCode = "200")
+    @PutMapping
+    public ResponseEntity<?> update(
+            @MemberId Long memberId,
+            @RequestBody MemberNicknameUpdateResponse memberNicknameUpdateResponse) {
         memberService.updateNickname(memberId, memberNicknameUpdateResponse);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(memberNicknameUpdateResponse, HttpStatus.OK);
     }
 }
