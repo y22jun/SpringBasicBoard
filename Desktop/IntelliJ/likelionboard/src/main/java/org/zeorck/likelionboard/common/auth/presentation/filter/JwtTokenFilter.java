a624a87da8042176f7e8c5af5f01063de3d87bbc
y22jun <tlsdpwns0612@naver.com>
2025-04-19 22:26:29 +0900

    Chore: 안 쓰는 import delete

--- Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/common/auth/presentation/filter/JwtTokenFilter.java
+++ Desktop/IntelliJ/likelionboard/src/main/java/org/zeorck/likelionboard/common/auth/presentation/filter/JwtTokenFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,9 +16,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.zeorck.likelionboard.common.auth.application.jwt.TokenInjector;
 import org.zeorck.likelionboard.common.auth.application.RefreshTokenService;
-import org.zeorck.likelionboard.common.auth.domain.jwt.TokenGenerator;
 import org.zeorck.likelionboard.common.auth.domain.jwt.TokenResolver;
-import org.zeorck.likelionboard.common.auth.infrastructure.jwt.JwtTokenGenerator;
 import org.zeorck.likelionboard.common.auth.presentation.exception.AuthenticationRequiredException;
 import org.zeorck.likelionboard.common.auth.presentation.exception.RefreshTokenNotValidaException;
 
